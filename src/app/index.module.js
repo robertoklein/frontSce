5bbe1ee921442c9d2e6d5ff7f58755262bcc9728
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-21 17:35:45 -0300

    Removendo módulo angular-touch.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('templateWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('templateWeb', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
 
 })();
