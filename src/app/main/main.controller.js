6b72235ab3b5b0237569fda05e4a2814f757c1b3
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-22 14:34:42 -0300

    Removendo componentes não utilizados.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,13 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController() {
     var vm = this;
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1487706585551;
-    vm.showToastr = showToastr;
+    vm.user = {
+      name: "Anderson Rodrigo Davi",
+      email: 'anderson.davi@pti.org.br'
+    };
 
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
