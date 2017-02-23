ea97a680cf54052c465fc60e0016b6206692ccb3
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-23 18:06:54 -0300

    Correções no sidenav.

--- src/app/template/template.controller.js
+++ src/app/template/template.controller.js
@@ -6,7 +6,7 @@
     .controller('TemplateController', TemplateController);
 
   /** @ngInject */
-  function TemplateController() {
+  function TemplateController($scope, $mdSidenav) {
     var vm = this;
 
     vm.user = {
@@ -14,5 +14,9 @@
       email: 'anderson.davi@pti.org.br'
     };
 
+    $scope.$on('$stateChangeSuccess', function() {
+      var sidenavLeft = $mdSidenav('sidenav-left');
+      sidenavLeft.close();
+    });
   }
 })();
