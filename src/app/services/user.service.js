234e4ed88a844dbbb6b3cbce4b1634a6a0473a0b
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-24 17:19:03 -0300

    Serviço de usuários.

---
+++ src/app/services/user.service.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  angular.module('templateWeb')
+    .factory('User', User)
+    .factory('UserService', UserService);
+
+  /** @ngInject */
+  function User($resource) {
+    return $resource('/api/users/:userId', {}, {
+      update: {method: 'PUT'}
+    });
+  }
+
+  /** @ngInject */
+  function UserService(User) {
+    return {
+      insert: insert,
+      update: update,
+      remove: remove,
+      query: query,
+      findOne: findOne
+    };
+
+    function insert(user) {
+      return User.save(user).$promise;
+    }
+
+    function update(user) {
+      return User.update(user).$promise;
+    }
+
+    function remove(id) {
+      return User.remove({userId: id}).$promise;
+    }
+
+    function query(params) {
+      return User.query(params).$promise;
+    }
+
+    function findOne(id) {
+      return User.get({userId: id}).$promise;
+    }
+  }
+
+})();
