231385d808639f34340afd2a1e35bd8dbdac6055
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-24 17:21:45 -0300

    Formulário de usuários.

--- src/app/settings/settings-users-form/settings-users-form.controller.js
+++ src/app/settings/settings-users-form/settings-users-form.controller.js
@@ -6,9 +6,24 @@
     .controller('SettingsUsersFormController', SettingsUsersFormController);
 
   /** @ngInject */
-  function SettingsUsersFormController() {
+  function SettingsUsersFormController(UserService, $mdToast, $state) {
     var vm = this;
 
+    vm.user = {};
 
+    vm.saveUser = saveUser;
+
+    function saveUser() {
+      UserService.insert(vm.user).then(function success(response) {
+        $mdToast.showSimple('Usuário cadastrado com sucesso');
+        $state.go('settings.users_list');
+      }).catch(function error() {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent('Erro ao cadastrar usuário')
+            .toastClass('md-warn')
+        )
+      });
+    }
   }
 })();
