c04e3869db6a0378dcfc164b8fca5be74c03f533
Anderson Rodrigo Davi <anderson.davi@pti.org.br>
2017-02-24 17:14:24 -0300

    Diretiva para feature-bar.

---
+++ src/app/components/feature-bar/feature-bar.directive.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('templateWeb')
+    .directive('appFeatureBar', appFeatureBar);
+
+  /** @ngInject */
+  function appFeatureBar() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/feature-bar/feature-bar.html',
+      scope: {
+        title: '@',
+        subtitle: '@',
+        srefBack: '@',
+        tabs: '='
+      },
+      controller: FeatureBarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    /** @ngInject */
+    function FeatureBarController($state) {
+      var vm = this;
+
+      vm.currentTab = $state.current.name;
+
+      vm.back = back;
+
+      function back() {
+        $state.go('^');
+      }
+
+    }
+  }
+
+})();
